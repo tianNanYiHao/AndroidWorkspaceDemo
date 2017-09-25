02b0a0bd8241af2f8d45a3efe67c0d5c127825d5
tianNanYiHao <851085835@qq.com>
2017-09-25 18:57:55 +0800

    20170925

--- space-android/Day02_06AndroidLayoutDemo/gen/com/qijia/day02_06androidlayoutdemo/R.java
+++ space-android/Day02_06AndroidLayoutDemo/gen/com/qijia/day02_06androidlayoutdemo/R.java
@@ -25,10 +25,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int textview1=0x7f080000;
+        public static final int textview2=0x7f080001;
+        public static final int textview3=0x7f080002;
+        public static final int textview4=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int framelayout=0x7f030001;
+        public static final int mylinerlayout=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
