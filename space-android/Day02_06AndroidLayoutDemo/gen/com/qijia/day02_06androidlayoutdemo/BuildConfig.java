bb4d0cdcacc915a5fad1dedfb584869a8a11bf4f
tianNanYiHao <851085835@qq.com>
2017-09-25 17:54:47 +0800

    删除空项目

---
+++ space-android/Day02_06AndroidLayoutDemo/gen/com/qijia/day02_06androidlayoutdemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.qijia.day02_06androidlayoutdemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
